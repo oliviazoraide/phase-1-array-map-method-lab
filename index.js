7a0ab1584ce4c2eb057db8951c6760e95616f2fd
Olivia Desgrossillier <oliviadesgrossillier@gmail.com>
2023-04-12 02:15:34 -0700

    completed map method lab

--- index.js
+++ index.js
@@ -12,5 +12,8 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((array) => {
+   return array.split(' ').map((word) => {
+    return word[0].toUpperCase() + word.slice(1)
+   }).join(' ')})
+  }
